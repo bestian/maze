037efa970c64a633d09fea30e1b83d9f3cbee628
Bestian Tang 小巴 <bestian@gmail.com>
2020-09-10 15:56:14 +0800

    Update Scene.vue

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="scene">
     <div id="ctrl"> 
-      <button @click="move('z', -5)"> 前 </button> 
-      <button @click="move('z', 5)"> 後 </button>
-      <button @click="move('x', -5)"> 左 </button> 
-      <button @click="move('x', 5)"> 右 </button>
+      <button id="up" @click="move('z', -5)" :class = "{active: m == 'up' }"> 前 </button> 
+      <button id="down" @click="move('z', 5)" :class = "{ active: m == 'down' }"> 後 </button>
+      <button id="left" @click="move('x', -5)" :class = "{ active: m == 'left' }"> 左 </button> 
+      <button id="right" @click="move('x', 5)" :class = "{ active: m == 'right' }"> 右 </button>
     </div>
-    <div id="win" v-show="win" @click="win = false">
+    <a id="win" v-show="win" @click="win = false">
       <img src="../assets/rainbow.jpg">
-    </div>
+    </a>
     <div id="three-scene-canvas"></div>
   </div>
   
@@ -31,7 +31,37 @@ export default {
       me: null,
       you: null,
       win: false,
-      audio: null
+      audio: null,
+      m: null,
+      lev: 0,
+    // 自訂迷宮
+    // 0: 空地
+    // 1: 牆
+    // 5: 起點
+    // 9: 終點
+      maze: [
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,1,1,1,0,1,1,1,0,1,1,1,5,1,0,1,1,1,1],
+        [1,0,0,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,0,1],
+        [1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1],
+        [1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,0,1,1,0,1],
+        [1,0,1,0,1,0,1,0,1,0,1,1,0,0,1,0,1,1,0,1],
+        [1,0,1,0,1,0,1,0,1,0,0,0,0,1,1,0,1,1,0,1],
+        [1,0,0,0,1,0,1,0,1,1,1,1,0,1,1,0,1,1,0,1],
+        [1,0,1,1,1,0,1,0,1,0,1,1,0,1,1,0,1,1,0,1],
+        [1,0,1,1,1,0,1,0,1,0,1,0,0,0,1,0,1,1,0,1],
+        [1,0,0,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,0,1],
+        [1,1,1,0,1,0,0,0,1,0,0,0,0,0,1,0,1,1,0,1],
+        [1,0,0,0,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1],
+        [1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,0,1,1,0,1],
+        [1,0,0,0,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1],
+        [1,0,1,1,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1],
+        [1,0,1,9,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1],
+        [1,0,1,0,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1],
+        [1,0,0,0,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+      ]
     }
   },
   created () {
@@ -82,37 +112,15 @@ export default {
     light.shadow.mapSize.height = 2048
     this.scene.add(light)
 
-    // Adding cubes
-    let material = new THREE.MeshPhysicalMaterial({color: 0x00ff00})
-    var BoxGeometry = new THREE.BoxGeometry(5, 2, 5)
-    var maze = [
-      [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-      [1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1],
-      [1,0,1,1,1,0,1,1,1,0,1,1,1,5,1,0,1,1,1,1],
-      [1,0,0,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,0,1],
-      [1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1],
-      [1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,0,1,1,0,1],
-      [1,0,1,0,1,0,1,0,1,0,1,1,0,0,1,0,1,1,0,1],
-      [1,0,1,0,1,0,1,0,1,0,0,0,0,1,1,0,1,1,0,1],
-      [1,0,0,0,1,0,1,0,1,1,1,1,0,1,1,0,1,1,0,1],
-      [1,0,1,1,1,0,1,0,1,0,1,1,0,1,1,0,1,1,0,1],
-      [1,0,1,1,1,0,1,0,1,0,1,0,0,0,1,0,1,1,0,1],
-      [1,0,0,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,0,1],
-      [1,1,1,0,1,0,0,0,1,0,0,0,0,0,1,0,1,1,0,1],
-      [1,0,0,0,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1],
-      [1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,0,1,1,0,1],
-      [1,0,0,0,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1],
-      [1,0,1,1,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1],
-      [1,0,1,9,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1],
-      [1,0,1,0,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1],
-      [1,0,0,0,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1],
-      [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-    ];
     this.meshArray = [];
-    for (var x=0; x<maze.length; x++) {
-        for(var z=0; z<maze[0].length; z++){
+    for (var x=0; x < this.maze.length; x++) {
+        for(var z=0; z <this.maze[0].length; z++){
           var mesh;
-          if (maze[x][z] === 1) {
+          // 牆壁
+          if (this.maze[x][z] === 1) {
+            let c = [0x00ff00, 0x00cc00, 0x009900][Math.floor(Math.random()*3)]
+            let material = new THREE.MeshPhysicalMaterial({color: c})
+            let BoxGeometry = new THREE.BoxGeometry(5, 5, 5)
             mesh = new THREE.Mesh(BoxGeometry, material.clone());
             this.meshArray.push(mesh);
             mesh.position.x = -5*x - 10;
@@ -120,19 +128,23 @@ export default {
             mesh.position.z = -5*z + 20;
             this.scene.add(mesh);
           }
-          if (maze[x][z] === 9) {
-            var Spheregeometry = new THREE.SphereGeometry( 1, 32, 32 );
-            var material2 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+
+          // 終點
+          if (this.maze[x][z] === 9) {
+            let Spheregeometry = new THREE.SphereGeometry( 1, 32, 32 );
+            let material2 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
             this.you = new THREE.Mesh( Spheregeometry, material2.clone() );
             this.you.position.x = -5*x - 10;
             this.you.position.y = -20;
             this.you.position.z = -5*z + 20;
             this.scene.add(this.you);  
           }
-          if (maze[x][z] === 5) {
-            var Spheregeometry2 = new THREE.SphereGeometry( 1, 32, 32 );
-            var material3 = new THREE.MeshBasicMaterial( {color: 0x00ff} );
-            this.me = new THREE.Mesh( Spheregeometry2, material3.clone() );
+
+          // 起點
+          if (this.maze[x][z] === 5) {
+            let Spheregeometry = new THREE.SphereGeometry( 1, 32, 32 );
+            let material3 = new THREE.MeshBasicMaterial( {color: 0x00ff} );
+            this.me = new THREE.Mesh( Spheregeometry, material3.clone() );
             this.me.position.x = -5*x - 10;
             this.me.position.y = -20;
             this.me.position.z = -5*z + 20;
@@ -141,7 +153,7 @@ export default {
         }
     }
     this.animateThreeJs();
-    window.addEventListener('keyup', this.keyup);
+    window.addEventListener('keyup', this.keyup); // 聽鍵盤事件
   },
   methods: {
     animateThreeJs () {
@@ -149,10 +161,14 @@ export default {
       this.renderer.shadowMap.needsUpdate = true
     },
     keyup(e) {
-      if (e.which === 37) { this.move('x', -5) }
-      if (e.which === 38) { this.move('z', -5) }
-      if (e.which === 39) { this.move('x', 5) }
-      if (e.which === 40) { this.move('z', 5) }
+      // Left key
+      if (e.which === 37) { this.move('x', -5); this.m = 'left' }
+      // up key
+      if (e.which === 38) { this.move('z', -5); this.m = 'up' }
+      // right key
+      if (e.which === 39) { this.move('x', 5); this.m = 'right' }
+      // down key
+      if (e.which === 40) { this.move('z', 5); this.m = 'down' }
     },
     move (xyz, int) {
       this.me.position[xyz] += int;
@@ -163,7 +179,7 @@ export default {
 
         if (position.x === this.me.position.x && position.z === this.me.position.z ) {
           this.me.position[xyz] -= int;
-        }
+        } // 如果撞牆就取消移動
 
         var xz = {x: this.me.position.x, z: this.me.position.z }
         
@@ -171,9 +187,11 @@ export default {
         this.controls.target.set(this.me.position.x, this.me.position.y, this.me.position.z);
         this.controls.update();
       }
+
       if (this.me.position.x === this.you.position.x && this.me.position.z === this.you.position.z ) {
         this.win = true
       }
+
       this.animateThreeJs();
     }
   }
@@ -184,7 +202,7 @@ export default {
 
 #ctrl {
   position: fixed;
-  bottom: 15px;
+  bottom: 150px;
   left: 0;
   z-index: 999;
 }
@@ -200,13 +218,49 @@ export default {
   position: fixed;
   top: 50px;
   left: 50px;
-  width: 80vw;
-  height: 80vh;
+  width: 50vw;
+  height: 50vh;
   cursor: pointer;
 }
 
 #win img {
-  width: 100%
+  cursor: pointer;
+  width: 100%;
 }
+
+#left, #right, #up, #down {
+  position: absolute;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.active {
+  background-color: blue;
+}
+
+#left, #right {
+  top: 0px;
+}
+
+#up, #down {
+  left: 50px;
+}
+
+#up {
+  top: -50px;
+}
+
+#down {
+  top: 50px;
+}
+
+#left {
+  left: 0
+}
+
+#right {
+  left: 100px;
+}
+
 
 </style>
