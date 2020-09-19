21755c05b259a4531ac7b3274046ceaa63540183
Bestian Tang <bestian@gmail.com>
2020-09-19 09:01:02 +0800

    use axois

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -39,36 +39,8 @@ export default {
     // 1: 牆
     // 5: 起點
     // 9: 終點
-      maze: [
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
-        [1,1,0,1,1,1,1,0,0,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1],
-        [1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1],
-        [1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,1,0,1,0,1],
-        [1,0,0,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,0,1,0,0,0,1,0,1,0,1],
-        [1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0,1,0,1,0,1,0,1],
-        [1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1],
-        [1,0,1,0,1,0,1,0,1,1,1,1,0,0,1,0,1,1,0,0,0,1,1,1,0,1,0,1],
-        [1,0,1,0,1,0,1,0,1,0,0,0,0,1,1,0,1,1,0,1,1,1,0,0,0,1,0,1],
-        [1,0,0,0,1,0,1,0,1,1,1,1,0,1,1,0,1,1,0,0,0,0,0,1,0,1,0,1],
-        [1,0,1,1,1,0,1,0,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,0,1],
-        [1,0,1,1,1,0,1,0,1,0,1,0,0,0,0,0,1,1,0,1,1,0,1,1,0,0,0,1],
-        [1,0,1,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,0,1,0,0,0,1,0,1,0,1],
-        [1,5,1,0,1,0,0,0,1,0,0,0,0,0,1,0,1,1,0,1,0,1,0,1,0,1,0,1],
-        [1,0,0,0,1,1,0,1,1,0,1,1,1,0,1,1,1,1,0,1,0,1,0,1,0,1,0,1],
-        [1,1,1,1,1,0,0,0,0,0,0,0,1,0,1,0,0,1,1,1,0,1,0,1,0,1,0,1],
-        [1,0,0,0,1,1,0,1,1,0,1,1,1,0,1,1,0,1,0,1,1,1,0,1,0,1,0,1],
-        [1,0,1,0,1,1,0,1,1,0,1,0,0,0,1,0,0,1,0,1,0,0,0,1,0,1,1,1],
-        [1,0,1,9,1,1,0,1,1,0,1,0,1,0,1,0,1,1,0,1,0,1,0,0,0,0,1,1],
-        [1,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,1,0,1,1],
-        [1,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,1,0,1,1,0,1,1],
-        [1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,0,1,1,1,1,0,0,1],
-        [1,0,1,0,0,0,0,0,0,1,1,0,1,0,0,0,1,0,0,0,0,1,1,1,1,1,1,1],
-        [1,0,1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1],
-        [1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1],
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-
-      ]
+      maze: null,
+      mazes: ['maze0.json','maze1.json','maze2.json']
     }
   },
   created () {
@@ -78,89 +50,92 @@ export default {
     /* **************
        BASIC SETUP
     ************** */
+    this.axios.get('./maze2.json').then((response) => {
+      console.log(response.data)
+      this.maze = response.data;
+      this.sceneCanvas = document.getElementById('three-scene-canvas')
+      this.scene = new THREE.Scene()
+      this.camera = new THREE.PerspectiveCamera(
+        75,
+        this.sceneCanvas.getBoundingClientRect().width / this.sceneCanvas.getBoundingClientRect().height,
+        0.1,
+        1000
+      )
+      this.camera.position.set(20, 5, -20)
+      
+      this.renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        powerPreference: "high-performance"
+      })
+      this.renderer.outputEncoding = THREE.sRGBEncoding
 
-    this.sceneCanvas = document.getElementById('three-scene-canvas')
-    this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      this.sceneCanvas.getBoundingClientRect().width / this.sceneCanvas.getBoundingClientRect().height,
-      0.1,
-      1000
-    )
-    this.camera.position.set(20, 5, -20)
-    
-    this.renderer = new THREE.WebGLRenderer({
-      antialias: true,
-      powerPreference: "high-performance"
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+      this.controls.addEventListener('change', this.animateThreeJs )
+
+      this.renderer.setSize(this.sceneCanvas.offsetWidth, this.sceneCanvas.offsetHeight)
+      this.renderer.setClearColor("#212121")
+      this.renderer.shadowMap.enabled = true
+      this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
+      this.renderer.shadowMapSoft = true
+      this.renderer.shadowMap.autoUpdate = false
+      this.renderer.shadowMap.needsUpdate = true
+      this.sceneCanvas.append(this.renderer.domElement)
+      
+      // lighting
+      let ambientLight = new THREE.AmbientLight (0xdaccff, 0.5)
+      this.scene.add(ambientLight)
+
+      let light = new THREE.PointLight(0xfc831d, 1, 100)
+      light.position.set(15, 10, 15)
+      light.castShadow = true
+      light.shadow.radius = 1
+      light.shadow.mapSize.width = 2048
+      light.shadow.mapSize.height = 2048
+      this.scene.add(light)
+
+      this.meshArray = [];
+      for (var x=0; x < this.maze.length; x++) {
+          for(var z=0; z <this.maze[0].length; z++){
+            var mesh;
+            // 牆壁
+            if (this.maze[x][z] === 1) {
+              let c = [0x3a406e, 0x00a6ff, 0xff003b][Math.floor(Math.random()*3)]
+              let material = new THREE.MeshPhysicalMaterial({color: c})
+              let BoxGeometry = new THREE.BoxGeometry(5, 2, 5)
+              mesh = new THREE.Mesh(BoxGeometry, material.clone());
+              this.meshArray.push(mesh);
+              mesh.position.x = -5*x - 10;
+              mesh.position.y = -20;
+              mesh.position.z = -5*z + 20;
+              this.scene.add(mesh);
+            }
+
+            // 終點
+            if (this.maze[x][z] === 9) {
+              let BoxGeometry = new THREE.BoxGeometry( 2, 50, 2);
+              let material2 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+              this.you = new THREE.Mesh( BoxGeometry, material2.clone() );
+              this.you.position.x = -5*x - 10;
+              this.you.position.y = 5;
+              this.you.position.z = -5*z + 20;
+              this.scene.add(this.you);  
+            }
+
+            // 起點
+            if (this.maze[x][z] === 5) {
+              let Spheregeometry = new THREE.SphereGeometry( 1, 32, 32 );
+              let material3 = new THREE.MeshBasicMaterial( {color: 0x00ff} );
+              this.me = new THREE.Mesh( Spheregeometry, material3.clone() );
+              this.me.position.x = -5*x - 10;
+              this.me.position.y = -20;
+              this.me.position.z = -5*z + 20;
+              this.scene.add(this.me);  
+            }
+          }
+      }
+      this.animateThreeJs();
+      window.addEventListener('keyup', this.keyup); // 聽鍵盤事件
     })
-    this.renderer.outputEncoding = THREE.sRGBEncoding
-
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    this.controls.addEventListener('change', this.animateThreeJs )
-
-    this.renderer.setSize(this.sceneCanvas.offsetWidth, this.sceneCanvas.offsetHeight)
-    this.renderer.setClearColor("#212121")
-    this.renderer.shadowMap.enabled = true
-    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
-    this.renderer.shadowMapSoft = true
-    this.renderer.shadowMap.autoUpdate = false
-    this.renderer.shadowMap.needsUpdate = true
-    this.sceneCanvas.append(this.renderer.domElement)
-    
-    // lighting
-    let ambientLight = new THREE.AmbientLight (0xdaccff, 0.5)
-    this.scene.add(ambientLight)
-
-    let light = new THREE.PointLight(0xfc831d, 1, 100)
-    light.position.set(15, 10, 15)
-    light.castShadow = true
-    light.shadow.radius = 1
-    light.shadow.mapSize.width = 2048
-    light.shadow.mapSize.height = 2048
-    this.scene.add(light)
-
-    this.meshArray = [];
-    for (var x=0; x < this.maze.length; x++) {
-        for(var z=0; z <this.maze[0].length; z++){
-          var mesh;
-          // 牆壁
-          if (this.maze[x][z] === 1) {
-            let c = [0x3a406e, 0x00a6ff, 0xff003b][Math.floor(Math.random()*3)]
-            let material = new THREE.MeshPhysicalMaterial({color: c})
-            let BoxGeometry = new THREE.BoxGeometry(5, 2, 5)
-            mesh = new THREE.Mesh(BoxGeometry, material.clone());
-            this.meshArray.push(mesh);
-            mesh.position.x = -5*x - 10;
-            mesh.position.y = -20;
-            mesh.position.z = -5*z + 20;
-            this.scene.add(mesh);
-          }
-
-          // 終點
-          if (this.maze[x][z] === 9) {
-            let BoxGeometry = new THREE.BoxGeometry( 2, 50, 2);
-            let material2 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-            this.you = new THREE.Mesh( BoxGeometry, material2.clone() );
-            this.you.position.x = -5*x - 10;
-            this.you.position.y = 5;
-            this.you.position.z = -5*z + 20;
-            this.scene.add(this.you);  
-          }
-
-          // 起點
-          if (this.maze[x][z] === 5) {
-            let Spheregeometry = new THREE.SphereGeometry( 1, 32, 32 );
-            let material3 = new THREE.MeshBasicMaterial( {color: 0x00ff} );
-            this.me = new THREE.Mesh( Spheregeometry, material3.clone() );
-            this.me.position.x = -5*x - 10;
-            this.me.position.y = -20;
-            this.me.position.z = -5*z + 20;
-            this.scene.add(this.me);  
-          }
-        }
-    }
-    this.animateThreeJs();
-    window.addEventListener('keyup', this.keyup); // 聽鍵盤事件
   },
   methods: {
     animateThreeJs () {
