1366f41e3609ae94adee33a2a3ec7165dd9d8a81
Bestian Tang <bestian@gmail.com>
2021-01-02 21:18:04 +0800

    deploy

---
+++ precache-manifest.bfb479a1f616919c1f8aca915feff00d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91b0bb1b541852e1667",
+    "url": "/maze/css/app.2a4dbd8e.css"
+  },
+  {
+    "revision": "bec44f894b327d8d1375b091cb0ce460",
+    "url": "/maze/happy.mp3"
+  },
+  {
+    "revision": "6276d8ecbc7bf0adddcf2ba7894cee16",
+    "url": "/maze/img/cat.6276d8ec.jpg"
+  },
+  {
+    "revision": "39a7754e3d1a3cd8d2c994cef87a6bed",
+    "url": "/maze/img/food.39a7754e.jpg"
+  },
+  {
+    "revision": "e98c9262defd9a845d6f3d7bb5e4ae82",
+    "url": "/maze/img/maze.png"
+  },
+  {
+    "revision": "b0bde22a08b3de902a49e2f129811935",
+    "url": "/maze/img/trapdoor.b0bde22a.png"
+  },
+  {
+    "revision": "a4c38481478d769e093fe25e5b8fe59c",
+    "url": "/maze/index.html"
+  },
+  {
+    "revision": "b91b0bb1b541852e1667",
+    "url": "/maze/js/app.d40957e6.js"
+  },
+  {
+    "revision": "bf996cbd72130f31944f",
+    "url": "/maze/js/chunk-vendors.95dd977b.js"
+  },
+  {
+    "revision": "7c4909c91ab6cf8c27a3bdf38f940c37",
+    "url": "/maze/manifest.json"
+  },
+  {
+    "revision": "9cf2ccd0e07f4df6726b6e9850ea11c3",
+    "url": "/maze/maze0.json"
+  },
+  {
+    "revision": "f083567e427db4e05792010c702db3c0",
+    "url": "/maze/maze1.json"
+  },
+  {
+    "revision": "3e369583e0f5d4540a585495dd1e896d",
+    "url": "/maze/maze2.json"
+  },
+  {
+    "revision": "a7907b2745249f9d7f2b2e1a886b004a",
+    "url": "/maze/pokemon.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/maze/robots.txt"
+  }
+]);
